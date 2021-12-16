b6888c6908d2ab510d742121a02595ffcbd729be
Jean Chiclla <j-c_f@outlook.com>
2021-12-16 11:16:20 -0500

    Se agregó un ícono y se cambió el nombre de la app

--- lib/global/environment.dart
+++ lib/global/environment.dart
@@ -1,14 +1,15 @@
-import 'dart:io';
-
 class Environment {
-  static String apiUrl = Platform.isAndroid
+  static String apiUrl =
+      // Platform.isAndroid
       // ? 'http://10.0.2.2:3001/api'
-      ? 'http://192.168.8.100:3000/api'
-      : 'http://localhost:3000/api';
+      // ? 'http://192.168.8.100:3000/api'
+      // : 'http://localhost:3000/api';
+      'https://flutter-chat-server-jp.herokuapp.com/api';
 
   static String socketUrl =
       // Platform.isAndroid ? 'http://10.0.2.2:3000' : 'http://localhost:3000';
-      Platform.isAndroid
-          ? 'http://192.168.8.100:3000/'
-          : 'http://localhost:3000/';
+      // Platform.isAndroid
+      //     ? 'http://192.168.8.100:3000/'
+      //     : 'http://localhost:3000/';
+      'https://flutter-chat-server-jp.herokuapp.com/';
 }
